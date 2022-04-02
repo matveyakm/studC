facd7380f9b4302938926a6877afde38e97dafb4
Матвей Акименко <matveyakm@gmail.com>
2022-04-02 21:04:51 +0700

    Добавьте файлы проекта.

---
+++ studC/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace studB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            double average = 0;
+            bool isFirst = true;
+            for (double R = 10000000; R >= 3000000; R--) // for (double R = 10000000; R >= 3000000; R--)
+            {
+                if (isPrime(R) == true && isPrime(R - 2) == true)
+                {
+                    count++;
+                    if (isFirst == true)
+                    {
+                        isFirst = false;
+                        average = R + (R - 2) / 2;
+                        Console.WriteLine(R);
+                    }
+                }
+            }
+
+            Console.WriteLine(count + "  " + average);
+        }
+        static bool isPrime(double x)
+        {
+            for (int y = 2; y <= Math.Sqrt(x); y++)
+            {
+                if (x % y == 0) return false;
+            }
+            return true;
+
+        }
+    }
+}
